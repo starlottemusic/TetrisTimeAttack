94960cd2cbf24815ade064a31063aeb26f1629be
habermd1 <habermd1@my.erau.edu>
2026-04-08 16:43:39 -0700

    working anim system

--- Source/Tutorial/Tutorial.c
+++ Source/Tutorial/Tutorial.c
@@ -16,11 +16,13 @@ void mainTutorial() {
 }
 
 void tickTutorial(byte tickCounter) {
-    if (tickCounter % 30 == 0) {
+    static int swaps;
+    if (tickCounter % 60 == 0) {
         frame++;
         if (frame == 4)
             frame = 0;
     }
+
     if (slide==0)
         mvprintw(22,3,"Exit");
     else
@@ -57,9 +59,9 @@ void tickTutorial(byte tickCounter) {
 
 char assignMatrix(int s, int f) {
     switch (s) {
-        case 0:
+        case 0: // Slide 0
             switch (f) {
-                case 0: {
+                case 0: { // Frame 0
                     char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
                         "BBBBOOBBBB",
                         "BBBBOOBBBB",
@@ -71,7 +73,7 @@ char assignMatrix(int s, int f) {
                     memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
                     break;
                 }
-                case 1: {
+                case 1: { // Frame 1
                     char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
                         "BBBBBBBBBB",
                         "BBBBBBBBBB",
@@ -83,64 +85,202 @@ char assignMatrix(int s, int f) {
                     memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
                     break;
                 }
-                case 2: {
+                case 2: { // Frame 2
+                    char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
+                        "BBBBBBBBBB",
+                        "BBBBBBBBBB",
+                        "BBSBBBBBBB",
+                        "BZSSBBBBBB",
+                        "++++++++++",
+                        "++++++++++",
+                        };
+                    memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
                     break;
                 }
-                case 3: {
+                case 3: { // Frame 3
+                    char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
+                        "BBBBBBBBBB",
+                        "BBBBBBBBBB",
+                        "BBBBBBBBBB",
+                        "BBBBBBBBBB",
+                        "BBSBBBBBBB",
+                        "BZSSBBBBBB",
+                        };
+                    memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
                     break;
                 }
                 default:
                     break;
             }
             break;
-
-        case 1:
+        case 1: // Slide 1
             switch (f) {
-                case 0:
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
+            case 0: { // Frame 0
+                char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
+                    "BBBBBBBBBB",
+                    "BBBBBBBBBB",
+                    "BBBBBBBBBB",
+                    "BBBBBBBBBB",
+                    "BBBBJBBBBB",
+                    "BBBBJJJBBB",
+                    };
+                memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
+                break;
+            }
+            case 1: { // Frame 1
+                char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
+                    "BBBBBBBBBB",
+                    "BBBBBBBBBB",
+                    "BBBBBBBBBB",
+                    "BBBBBOOBBB",
+                    "BBBBJOOBBB",
+                    "BBBBJJJBBB",
+                    };
+                memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
+                break;
+            }
+            case 2: { // Frame 2
+                char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
+                    "BBBBBBBBBB",
+                    "BBBBBBBBBB",
+                    "BBBBLLLBBB",
+                    "BBBBLOOBBB",
+                    "BBBBJOOBBB",
+                    "BBBBJJJBBB",
+                    };
+                memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
+                break;
+            }
+            case 3: { // Frame 3
+                char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
+                    "BBBBBBBBBB",
+                    "BBBBBBBBBB",
+                    "BBBILLLBBB",
+                    "BBBILOOBBB",
+                    "BBBIJOOBBB",
+                    "BBBIJJJBBB",
+                    };
+                memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
+                break;
+            }
                 default:
                     break;
             }
             break;
-
-        case 2:
+        case 2: // Slide 2
             switch (f) {
-                case 0:
+                case 0: { // Frame 0
+                    char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
+                        "BBBBBBBBBB",
+                        "BBBBBBBBBB",
+                        "BBBBBBBBBB",
+                        "BBBBBBBBBB",
+                        "BBBBBBBBBB",
+                        "BBBIIIIBBB",
+                        };
+                    memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
                     break;
-                case 1:
+                }
+            case 1: { // Frame 1
+                    char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
+                        "BBBBBBBBBB",
+                        "BBBBBBBBBB",
+                        "BBBBBBBBBB",
+                        "BBBB+BBBBB",
+                        "BBBB+++BBB",
+                        "BBBIIIIBBB",
+                        };
+                    memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
                     break;
-                case 2:
+            }
+            case 2: { // Frame 2
+                    char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
+                        "BBBBBBBBBB",
+                        "BBBBBBBBBB",
+                        "BBBBBBBBBB",
+                        "BBBBJBLLBB",
+                        "BBBBJJJLBB",
+                        "BBBIIIILBB",
+                        };
+                    memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
                     break;
-                case 3:
+            }
+            case 3: { // Frame 3
+                    char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
+                        "BBBBBBBBBB",
+                        "BBBBB++BBB",
+                        "BBBBB++BB",
+                        "BBBBJBLLBB",
+                        "BBBBJJJLBB",
+                        "BBBIIIILBB",
+                        };
+                    memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
                     break;
-                default:
+            }
+            default:
+                    break;
+            }
+        case 3: // Slide 3
+            switch (f) {
+            case 0: { // Frame 0
+                char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
+                    "B+BJBBBB+",
+                    "B+BJJJBB+",
+                    "B+BBBBBB+",
+                    "B+BIIIIB+",
+                    "B+BBBBBB+",
+                    "B+BTTTBB+",
+                    };
+                memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
+                mvprintw(10,2,"Oldest ->");
+                break;
+            }
+            case 1: { // Frame 1
+                char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
+                    "B+BJBBBB+",
+                    "B+BJJJBB+",
+                    "B+BBBBBB+",
+                    "B+BIIIIB+",
+                    "B+BBBBBB+",
+                    "B+BTTTBB+",
+                    };
+                memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
+                mvprintw(10,2,"         ");
+                mvprintw(13,2,"Newest ->");
+                break;
+            }
+            case 2: { // Frame 2
+                char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
+                    "B+BJBBBB+",
+                    "B+BJJJBB+",
+                    "B+BBBBBB+",
+                    "B+BIIIIB+",
+                    "B+BBBBBB+",
+                    "B+BTTTBB+",
+                    };
+                memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
+                mvprintw(13,2,"         ");
+                mvprintw(18,10,"[LINE CLEAR]");
+                break;
+            }
+            case 3: { // Frame 3
+                char temp[SLIDE_HEIGHT][SLIDE_WIDTH] = {
+                    "B+BIIIIB+",
+                    "B+BBBBBB+",
+                    "B+BTTTBB+",
+                    "B+BBTBB+",
+                    "B+BBBBBB+",
+                    "B+BZZBB+",
+                    };
+                memcpy(mat, temp, SLIDE_HEIGHT*SLIDE_WIDTH);
+                mvprintw(18,11,"            ");
+                break;
+            }
+            default:
                     break;
             }
             break;
-
-        case 3:
-            switch (f) {
-                case 0:
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                default:
-                    break;
-            }
-            break;
-
         default:
             break;
-
     }
 }
